3d41aa6e0447947596109e5fbc08641f160a5735
E-taku <tkymtk8270@gmail.com>
2022-10-27 18:36:18 +0900

    create binary-search

---
+++ Search/binarysearch_01.py
@@ -0,0 +1,20 @@
+class BinarySearch_01:
+    def __init__(self,x: int,A: list):
+        self.x = x # 探したい値
+        self.A = sorted(A) # 配列
+        self.N = len(A)
+        self.idx = self.search()
+
+    def search(self):
+        L = 0
+        R = self.N - 1
+
+        while L <= R:
+            M = (L + R) // 2
+            if self.x < self.A[M]:
+                R = M - 1
+            if self.x == self.A[M]:
+                return M + 1
+            if self.x > self.A[M]:
+                L = M + 1
+        return -1
---
+++ Search/binarysearch_02.py
@@ -0,0 +1,23 @@
+# bisectでも同じことが可能
+
+class BinarySearch_1:
+    def __init__(self,x: int,A: list):
+        self.x = x # 探したい値
+        # self.A = sorted(A) # 配列
+        self.A = A # 配列
+        self.N = len(A)
+        self.idx = self.search()
+
+    def search(self):
+        L = 0
+        R = self.N - 1
+
+        while L <= R:
+            M = (L + R) // 2
+            if self.x < self.A[M]:
+                R = M - 1
+            if self.x == self.A[M]:
+                return M
+            if self.x > self.A[M]:
+                L = M + 1
+        return L
