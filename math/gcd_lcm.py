3c5fce2523fa555544320b9be182834242600f20
E-taku <tkymtk8270@gmail.com>
2022-11-04 09:56:41 +0900

    fix gcd,lcm

--- math/gcd_lcm.py
+++ math/gcd_lcm.py
@@ -4,12 +4,11 @@ class Gcd_Lcm:
     def __init__(self,a: int,b: int) -> None:
         self.a = a
         self.b = b
-        self.gcd = self.get_GCD()
-        self.lcm = self.get_LCM()
+        self.gcd = self.get_GCD(self.a, self.b)
+        self.lcm = self.get_LCM(self.a, self.b)
 
     # 2値の最大公約数を求める
-    def get_GCD(self) -> int:
-        a,b = self.a,self.b
+    def get_GCD(self, a: int, b: int) -> int:
         while a >= 1 and b >= 1:
             if a >= b:
                 a = a % b
@@ -20,7 +19,6 @@ class Gcd_Lcm:
         return b
 
     # 2値の最小公倍数を求める
-    def get_LCM(self) -> int:
-        a,b = self.a,self.b
+    def get_LCM(self, a: int, b: int) -> int:
         lcm = (a * b) // self.gcd
         return lcm
